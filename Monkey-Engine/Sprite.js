f4c1dbe268a445683f423fae3dbda5b0fa79f6a7
Your Name <simon.bendl@student.gyarab.cz>
2024-11-27 00:06:16 +0100

    Sprite being finalized

--- Monkey-Engine/Sprite.js
+++ Monkey-Engine/Sprite.js
@@ -11,22 +11,36 @@ export class Sprite {
         this._ctxCache = null; 
         this._isLoaded = false; 
           
-
         if (spritePath) {
             this.loadImg(spritePath);
         }
     }
 
     render(ctx) {
-        this._ctxCache = ctx;  // Uložíme ctx pro případ, že obrázek není načten
+        // Uložíme ctx pro případ, že obrázek není načten
+        this._ctxCache = ctx;  
 
         // Pokud je obrázek načten, vykreslíme ho
-        if (this._sprite && this._isLoaded) {
-            ctx.drawImage(this._sprite, this._x, this._y, this._width, this._height);
+        if (this._sprite) {
+            if (this._isLoaded){
+                ctx.drawImage(
+                    this._sprite, this._x, this._y, this._width, this._height
+                );
+            }else{
+                let id = null;
+                if (this._spritePath){id = this._spritePath}
+                else if (this._color){id = this._color}
+                else {id = "není zadán obrázek ani barva"}
+                console.error("Sprite s id: " + id + "\n" + " nemá načtený img" );
+
+            }
         }    
         // Pokud není načten, vykreslíme náhradní obdélník    
         else if (this._color) {
             ctx.fillStyle = this._color;
+            ctx.fillRect(this._x, this._y, this._width, this._height);
+        }else {
+            ctx.fillStyle = 'purple';
             ctx.fillRect(this._x, this._y, this._width, this._height);
         }
     }
