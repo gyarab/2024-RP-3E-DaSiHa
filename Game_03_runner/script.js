f9ad216caa87f36da10c486cf4650fd93700518d
Hana Hajná <117266542+HanaHajna7@users.noreply.github.com>
2024-12-04 19:48:30 +0000

    game3

---
+++ Game_03_runner/script.js
@@ -0,0 +1,13 @@
+const canvas = document.createElement('canvas');
+canvas.width = 700;
+canvas.height = 600;
+
+document.body.appendChild(canvas);
+const ctx = canvas.getContext('2d');
+const img = new Image();
+
+img.src = 'hra.jpg';
+
+img.onload = function() {
+    ctx.drawImage(img, 0, 0, 700, 600);
+};
