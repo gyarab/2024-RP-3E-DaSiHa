0dc9e796257555d174b407a1b5fe46b47bb25b0b
bendl-simon <simon.bendl@student.gyarab.cz>
2024-12-05 11:29:18 +0100

    canvas example

--- Game_03_runner/script.js
+++ Game_03_runner/script.js
@@ -7,7 +7,20 @@ import { CharacterSprite1 } from '../Monkey-Engine/CharacterSprite1.js';
 const pozadi = new Sprite(0, 0, 1080, 720);
 pozadi.loadImg("hra.jpg");
 
-const character = new Sprite(100, 500, 120, 100);
+const character = new   CharacterSprite1(120, 480, 75, 110);
 
-pozadi.render(ctx);
-character.render(ctx);
+character._id = "dinosaurus";
+character._framesRunning = [
+    "/Game_01_Ledvadva/sprites/runRight_0.png",
+    "/Game_01_Ledvadva/sprites/runRight_1.png",
+    "/Game_01_Ledvadva/sprites/runRight_2.png",
+    "/Game_01_Ledvadva/sprites/runRight_3.png"
+]
+
+//hlavní herní smyčka
+function Mainloop(){
+    pozadi.render(ctx);
+    character.render(ctx);
+    character.updatePos();
+}
+window.setInterval(Mainloop, 1, true);
