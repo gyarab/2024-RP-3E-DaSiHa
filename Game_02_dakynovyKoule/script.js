fea03b7bf32642b5bfe8ec70ac0c7ec6d3f62437
David Kyndl <david.kyndl@student.gyarab.cz>
2024-12-18 20:50:24 +0100

    lehkej adition

--- Game_02_dakynovyKoule/script.js
+++ Game_02_dakynovyKoule/script.js
@@ -1,17 +1,10 @@
-const canvas = document.getElementById('game');
+const canvas = document.getElementById('herniRozhraní');
 const ctx = canvas.getContext('2d');
 
 import { Sprite }           from '../Monkey-Engine/Sprite.js';
-import { CharacterSprite1 } from '../Monkey-Engine/CharacterSprite1.js';
+//import { CharacterSprite1 } from '../Monkey-Engine/CharacterSprite1.js';
 
-const pozadi = new Sprite(0, 0, 1080, 720);
-pozadi.loadImg("bowling.png");
-
-const character = new   CharacterSprite1(120, 480, 75, 110);
+const background = new Sprite(0, 0, 1920, 1100, "/Game_02_dakynovyKoule/bowling.png");
+background.render(ctx);
 
 
-//hlavní herní smyčka
-function Mainloop(){
-    pozadi.render(ctx);
-}
-window.setInterval(Mainloop, 1, true);
