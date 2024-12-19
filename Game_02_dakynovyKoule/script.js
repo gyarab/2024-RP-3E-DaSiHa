3f04c3cead7f444b5391261ac604f6dbcf8ffe0d
David Kyndl <david.kyndl@student.gyarab.cz>
2024-12-19 18:28:38 +0100

    Vypadato lowkey cool ale du to prekopat

--- Game_02_dakynovyKoule/script.js
+++ Game_02_dakynovyKoule/script.js
@@ -12,14 +12,16 @@ const character = new CharacterSprite1(790, 600, 350, 400);
 character._id = "sipka";
 character._framesRunning = [
     "/Game_02_dakynovyKoule/sipecka.png",
-    "/Game_02_dakynovyKoule/sipecka1.png", 
-    "/Game_02_dakynovyKoule/sipecka (1).png"
+    "/Game_02_dakynovyKoule/sipecka-1.png", 
+    "/Game_02_dakynovyKoule/sipecka-2.png", 
+    "/Game_02_dakynovyKoule/sipecka-3.png",
+    "/Game_02_dakynovyKoule/sipecka-4.png"
 ]
 
 
 function Mainloop(){
-    character.render(ctx, true);
+    character.render(ctx);
     character.updatePos();
 }
-window.setInterval(Mainloop, 5, true);
+window.setInterval(Mainloop, 20, true);
 
