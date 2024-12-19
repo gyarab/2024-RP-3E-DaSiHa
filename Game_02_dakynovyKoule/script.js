260630e087b0231f8aec0f8ce3d5a609868d730b
David Kyndl <david.kyndl@student.gyarab.cz>
2024-12-19 18:17:11 +0100

    Ma to nejaky tweaksy :(

--- Game_02_dakynovyKoule/script.js
+++ Game_02_dakynovyKoule/script.js
@@ -2,9 +2,24 @@ const canvas = document.getElementById('herniRozhraní');
 const ctx = canvas.getContext('2d');
 
 import { Sprite }           from '../Monkey-Engine/Sprite.js';
-//import { CharacterSprite1 } from '../Monkey-Engine/CharacterSprite1.js';
+import { CharacterSprite1 } from '../Monkey-Engine/CharacterSprite1.js';
 
 const background = new Sprite(0, 0, 1920, 1100, "/Game_02_dakynovyKoule/bowling.png");
 background.render(ctx);
 
+const character = new CharacterSprite1(790, 600, 350, 400);
+
+character._id = "sipka";
+character._framesRunning = [
+    "/Game_02_dakynovyKoule/sipecka.png",
+    "/Game_02_dakynovyKoule/sipecka1.png", 
+    "/Game_02_dakynovyKoule/sipecka (1).png"
+]
+
+
+function Mainloop(){
+    character.render(ctx, true);
+    character.updatePos();
+}
+window.setInterval(Mainloop, 5, true);
 
