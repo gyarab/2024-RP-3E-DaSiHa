dbd9412d0b2caa49a3b4b7b9b9a3692dd13e0b63
David Kyndl <david.kyndl@student.gyarab.cz>
2024-12-19 18:41:05 +0100

    Ok this lowkey vpohode

--- Game_02_dakynovyKoule/script.js
+++ Game_02_dakynovyKoule/script.js
@@ -4,8 +4,8 @@ const ctx = canvas.getContext('2d');
 import { Sprite }           from '../Monkey-Engine/Sprite.js';
 import { CharacterSprite1 } from '../Monkey-Engine/CharacterSprite1.js';
 
-const background = new Sprite(0, 0, 1920, 1100, "/Game_02_dakynovyKoule/bowling.png");
-background.render(ctx);
+const background = new Sprite(0, 0, 1920, 1100);
+background.loadImg("/Game_02_dakynovyKoule/bowling.png");
 
 const character = new CharacterSprite1(790, 600, 350, 400);
 
@@ -15,13 +15,27 @@ character._framesRunning = [
     "/Game_02_dakynovyKoule/sipecka-1.png", 
     "/Game_02_dakynovyKoule/sipecka-2.png", 
     "/Game_02_dakynovyKoule/sipecka-3.png",
-    "/Game_02_dakynovyKoule/sipecka-4.png"
+    "/Game_02_dakynovyKoule/sipecka-4.png", 
+    "/Game_02_dakynovyKoule/sipecka-3.png",
+    "/Game_02_dakynovyKoule/sipecka-2.png",
+    "/Game_02_dakynovyKoule/sipecka-1.png", 
+    "/Game_02_dakynovyKoule/sipecka.png", 
+    "/Game_02_dakynovyKoule/sipecka1.png",
+    "/Game_02_dakynovyKoule/sipecka2.png",
+    "/Game_02_dakynovyKoule/sipecka3.png",
+    "/Game_02_dakynovyKoule/sipecka4.png",
+    "/Game_02_dakynovyKoule/sipecka3.png",
+    "/Game_02_dakynovyKoule/sipecka2.png",
+    "/Game_02_dakynovyKoule/sipecka1.png",
+    "/Game_02_dakynovyKoule/sipecka.png"
+
 ]
 
 
 function Mainloop(){
+    background.render(ctx);
     character.render(ctx);
     character.updatePos();
 }
-window.setInterval(Mainloop, 20, true);
+window.setInterval(Mainloop, 6, true);
 
