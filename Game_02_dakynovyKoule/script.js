4a63ecc74a9a2b607b8b5d3dc2503a62911e5b93
David Kyndl <david.kyndl@student.gyarab.cz>
2024-12-19 18:53:52 +0100

    Jeste sem pridal kuzelky at se simonkovy hezky spi

--- Game_02_dakynovyKoule/script.js
+++ Game_02_dakynovyKoule/script.js
@@ -7,6 +7,28 @@ import { CharacterSprite1 } from '../Monkey-Engine/CharacterSprite1.js';
 const background = new Sprite(0, 0, 1920, 1100);
 background.loadImg("/Game_02_dakynovyKoule/bowling.png");
 
+const kuzelka1 = new Sprite(896, 155, 100, 90);
+kuzelka1.loadImg("/Game_02_dakynovyKoule/kuzelka.png");
+
+const kuzelka2 = new Sprite(857, 152, 100, 90);
+kuzelka2.loadImg("/Game_02_dakynovyKoule/kuzelka.png");
+
+const kuzelka3 = new Sprite(935, 152, 100, 90);
+kuzelka3.loadImg("/Game_02_dakynovyKoule/kuzelka.png");
+
+const kuzelka4 = new Sprite(818, 150, 100, 90);
+kuzelka4.loadImg("/Game_02_dakynovyKoule/kuzelka.png");
+
+const kuzelka7 = new Sprite(974, 150, 100, 90);
+kuzelka7.loadImg("/Game_02_dakynovyKoule/kuzelka.png");
+
+const kuzelka8 = new Sprite(779, 145, 100, 90);
+kuzelka8.loadImg("/Game_02_dakynovyKoule/kuzelka.png");
+
+const kuzelka10 = new Sprite(1013, 145, 100, 90);
+kuzelka10.loadImg("/Game_02_dakynovyKoule/kuzelka.png");
+
+
 const character = new CharacterSprite1(790, 600, 350, 400);
 
 character._id = "sipka";
@@ -34,6 +56,13 @@ character._framesRunning = [
 
 function Mainloop(){
     background.render(ctx);
+    kuzelka1.render(ctx);
+    kuzelka2.render(ctx);
+    kuzelka3.render(ctx);
+    kuzelka4.render(ctx);
+    kuzelka7.render(ctx);
+    kuzelka8.render(ctx);
+    kuzelka10.render(ctx);
     character.render(ctx);
     character.updatePos();
 }
