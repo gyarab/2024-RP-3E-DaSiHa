598db5885ca458b73ac81c5025797dc3592a55e7
Your Name <simon.bendl@student.gyarab.cz>
2024-12-31 19:00:06 +0100

    bottles

--- Game_01_Ledvadva/main.js
+++ Game_01_Ledvadva/main.js
@@ -1,13 +1,67 @@
-import { CharacterSprite2} from '../Monkey-Engine/CharacterSprite2.js';
-import { SpriteDyna } from '../Monkey-Engine/SpriteDyna.js';
-import { Tetragon } from '../Monkey-Engine/Tetragon.js';
+import { CharacterSprite2} from   '../Monkey-Engine/CharacterSprite2.js';
+import { SpriteDyna } from   '../Monkey-Engine/SpriteDyna.js';
+import { SpriteAnim } from   '../Monkey-Engine/SpriteAnim.js';
+import { Tetragon }   from   '../Monkey-Engine/Tetragon.js';
+import { Sprite }     from '../Monkey-Engine/Sprite.js';
 
 const canvas = document.getElementById('herniRozhraní');
 const ctx = canvas.getContext('2d');
 
-window.addEventListener('keydown', event => handleKey(event, true));
-window.addEventListener('keyup', event => handleKey(event, false));
+window.addEventListener('keydown', event => handleKey(event,  true));
+window.addEventListener('keyup'  , event => handleKey(event, false));
+/*-------------------------Obstacles------------------------------*/
+const w1 = new Tetragon(
+    {x: 200, y:   0},
+    {x: 600, y:   0},
+    {x: 600, y: 300},
+    {x: 200, y: 300},
+    "grey"
+)
+const w2 = new Tetragon(
+    {x: 200, y:  50},
+    {x: 900, y:  50},
+    {x: 900, y: 150},
+    {x: 200, y: 150},
+    "grey"
+)
+const w3 = new Tetragon(
+    {x: 200, y:  50},
+    {x: 400, y:  50},
+    {x: 400, y: 150},
+    {x: 200, y: 150},
+    "grey"
+)
+const w4 = new Tetragon(
+    {x: 200, y:  50},
+    {x: 400, y:  50},
+    {x: 400, y: 100},
+    {x: 200, y: 100},
+    "grey"
+)
+const w5 = new Tetragon(
+    {x:   0, y:  0},
+    {x:1500, y:  0},
+    {x:1500, y:200},
+    {x:   0, y:200},
+    "grey"
+)
+const w6 = new Tetragon(
+    {x:   0, y:   0},
+    {x:   0, y: 400},
+    {x: 250, y: 400},
+    {x: 250, y:   0},
+    "grey"
+)
 
+w1.moveTo(0     ,   400);
+w2.moveTo(650   ,   600);
+w3.moveTo(950   ,   500);
+w4.moveTo(650   ,   320);
+w5.moveTo(0     ,   900);
+w6.moveTo(1500  ,   760);
+
+const obsticles  = [w1, w2, w3, w4, w5, w6];
+/*-------------------------player1--------------------------------*/
 const player1 = new CharacterSprite2(150, 100, 68, 124,[
     //0
     "/Game_01_Ledvadva/sprites/BLU/stand.png",
@@ -57,7 +111,6 @@ const player1 = new CharacterSprite2(150, 100, 68, 124,[
 ]);
 
 player1._id = "player1";
-
 player1._framesStanding     = [player1._frames[0]];
 player1._framesRunRight     = player1._frames.slice(1, 15);
 player1._framesRunLeft      = player1._frames.slice(15, 29);
@@ -65,7 +118,7 @@ player1._framesJumpFarRight = player1._frames.slice(29, 32);
 player1._framesJumpFarLeft  = player1._frames.slice(32, 35);
 player1._framesPushRight    = [player1._frames[35]];
 player1._framesPushLeft     = [player1._frames[36]];
-
+/*------------------------player2---------------------------------*/
 const player2 = new CharacterSprite2(200, 100, 68, 124,[
     //0
     "/Game_01_Ledvadva/sprites/RED/stand.png",
@@ -114,7 +167,6 @@ const player2 = new CharacterSprite2(200, 100, 68, 124,[
 ]);
 
 player2._id = "player2"
-
 player2._framesStanding     = [player2._frames[0]];
 player2._framesRunRight     = player2._frames.slice(1, 15);
 player2._framesRunLeft      = player2._frames.slice(15, 29);
@@ -122,8 +174,8 @@ player2._framesJumpFarRight = player2._frames.slice(29, 32);
 player2._framesJumpFarLeft  = player2._frames.slice(32, 35);
 player2._framesPushRight    = [player2._frames[35]];
 player2._framesPushLeft     = [player2._frames[36]];
-
-const player3 = new SpriteDyna( 0, 1080 - 224, 120, 224, [
+/*--------------------------NPC--------------------------------*/
+const npc = new SpriteDyna( 0, 1080 - 168, 90, 168, [
     "/Game_01_Ledvadva/sprites/Alexa/1.png",
     "/Game_01_Ledvadva/sprites/Alexa/2.png",
     "/Game_01_Ledvadva/sprites/Alexa/3.png",
@@ -132,64 +184,54 @@ const player3 = new SpriteDyna( 0, 1080 - 224, 120, 224, [
     "/Game_01_Ledvadva/sprites/Alexa/6.png",
     "/Game_01_Ledvadva/sprites/Alexa/7.png",
     "/Game_01_Ledvadva/sprites/Alexa/8.png",
-    "/Game_01_Ledvadva/sprites/Alexa/7.png",
-    "/Game_01_Ledvadva/sprites/Alexa/6.png",
-    "/Game_01_Ledvadva/sprites/Alexa/5.png",
-    "/Game_01_Ledvadva/sprites/Alexa/4.png",
-    "/Game_01_Ledvadva/sprites/Alexa/3.png",
-    "/Game_01_Ledvadva/sprites/Alexa/2.png"
 ]);
-player3.animSlow = 30;
-const w1 = new Tetragon(
-    {x: 200, y:  50},
-    {x: 600, y:  50},
-    {x: 600, y: 400},
-    {x: 200, y: 400},
-    "orange"
-)
-const w2 = new Tetragon(
-    {x: 200, y:  50},
-    {x: 600, y:  50},
-    {x: 600, y: 150},
-    {x: 200, y: 150},
-    "grey"
-)
-const w3 = new Tetragon(
-    {x: 200, y:  50},
-    {x: 400, y:  50},
-    {x: 400, y: 150},
-    {x: 200, y: 150},
-    "grey"
-)
-const w4 = new Tetragon(
-    {x: 200, y:  50},
-    {x: 400, y:  50},
-    {x: 400, y: 100},
-    {x: 200, y: 100},
-    "grey"
-)
-w1.moveTo(0     ,   400);
-w2.moveTo(700   ,   620);
-w3.moveTo(950   ,   500);
-w4.moveTo(650   ,   300);
-
-const obsticles  = [w1, w2, w3, w4];
-player3._isGoRight= true;
-//hlavní herní smyčka
+npc.animSlow = 30;
+npc._isGoRight= true;
+/*------------------------Bottles---------------------------------*/
+const bottleBLU = new Sprite( null, null, 100, 100,
+    "/Game_01_Ledvadva/sprites/BLU/b.png"
+);
+bottleBLU.moveTo(720, 220);
+const bottleRED = new Sprite( null, null, 100, 100,
+    "/Game_01_Ledvadva/sprites/BLU/b.png"
+);
+bottleRED.moveTo(400, 800);
+const bottleORA = new Sprite( null, null, 100, 100,
+    "/Game_01_Ledvadva/sprites/BLU/b.png"
+);
+bottleORA.moveTo( 1000, 400);
+const bottleGRE = new Sprite( null, null, 100, 100,
+    "/Game_01_Ledvadva/sprites/BLU/b.png"
+);
+bottleGRE.moveTo( 500, 800);
+const bottleGRY = new Sprite( null, null, 100, 100,
+    "/Game_01_Ledvadva/sprites/BLU/b.png"
+);
+bottleGRY.moveTo( 1000, 800);
+const bottles = [bottleBLU, bottleRED, bottleORA, bottleGRE, bottleGRY];
+/*----------------------hlavní herní smyčka-----------------------*/
 function Mainloop() {
+
     ctx.clearRect(0,0,canvas.width, canvas.height);
+
     obsticles.forEach(obsticle => obsticle.render(ctx, true));
 
-    player3.render(ctx);
-    player3.updateAll();
-    if (player3._x > 1920) player3._x = -120;
+    npc.render(ctx);
+    npc.updateAll();
+    if (npc._x > 1920) npc._x = -120;
+
+    bottles.forEach(bottles => bottles.render(ctx));
+    
+
     player1.updatePos(obsticles);
     player1.updateImage();
     player1.render(ctx,0);
 
+    /*
     player2.updatePos(obsticles);
     player2.updateImage();
     player2.render(ctx,1);
+    */
 }
 window.setInterval(Mainloop, 6, true);
 
@@ -200,7 +242,7 @@ function handleKey(event, isDown) {
         'w': () => player1._wantJump    = isDown,
         'a': () => player1._wantGoLeft  = isDown ,
         'd': () => player1._wantGoRight = isDown ,
-        'r': () => player3._isGoRight   = isDown,
+        'r': () => npc._isGoRight   = isDown,
 
         'ArrowUp'   : () => player2._wantJump    = isDown,   
         'ArrowLeft' : () => player2._wantGoLeft  = isDown,
