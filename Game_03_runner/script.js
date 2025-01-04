38b0e8af895f3b618505e9b215f07af63175aff4
hanickaa <162619898+hanickaa@users.noreply.github.com>
2025-01-04 16:35:26 +0100

    pes

--- Game_03_runner/script.js
+++ Game_03_runner/script.js
@@ -11,6 +11,15 @@ const character = new CharacterSprite1(120, 690, 185, 210);
 
 character._id = "dinosaurus";
 character._framesRunning = [
+    "/Game_03_runner/sprites/faze10.png",
+    "/Game_03_runner/sprites/faze9.png",
+    "/Game_03_runner/sprites/faze8.png",
+    "/Game_03_runner/sprites/faze7.png",
+    "/Game_03_runner/sprites/faze6.png",
+    "/Game_03_runner/sprites/faze5.png",
+    "/Game_03_runner/sprites/faze4.png",
+    "/Game_03_runner/sprites/faze3.png",
+    "/Game_03_runner/sprites/faze2.png",
     "/Game_03_runner/sprites/faze1.png",
 ]
 
