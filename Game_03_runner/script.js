f57b95299b9a6877811c6eca4527cd6e8f0b25d6
hanickaa <162619898+hanickaa@users.noreply.github.com>
2025-01-04 12:49:56 +0100

    new sprite

--- Game_03_runner/script.js
+++ Game_03_runner/script.js
@@ -4,18 +4,14 @@ const ctx = canvas.getContext('2d');
 import { Sprite }           from '../Monkey-Engine/Sprite.js';
 import { CharacterSprite1 } from '../Monkey-Engine/CharacterSprite1.js';
 
-const pozadi = new Sprite(0, 0, 1080, 720);
+const pozadi = new Sprite(0, 0, 1915, 1080);
 pozadi.loadImg("hra.jpg");
 
-const character = new CharacterSprite1(120, 480, 75, 110);
+const character = new CharacterSprite1(120, 690, 185, 210);
 
 character._id = "dinosaurus";
 character._framesRunning = [
-    "/Game_03_runner/sprites/runner1.png",
-    "/Game_03_runner/sprites/runner2.png",
-    "/Game_03_runner/sprites/runner3.png",
-    "/Game_03_runner/sprites/runner4.png",
-    "/Game_03_runner/sprites/runner5.png",
+    "/Game_03_runner/sprites/faze1.png",
 ]
 
 //hlavní herní smyčka
