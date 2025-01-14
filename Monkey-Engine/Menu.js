fb0320146a8e62c6fdea49012f0041a343d86f5f
bendl-simon <simon.bendl@student.gyarab.cz>
2025-01-14 15:20:17 +0100

    Menu update

--- Monkey-Engine/Menu.js
+++ Monkey-Engine/Menu.js
@@ -9,29 +9,28 @@ export class Menu {
         this._rows  = rows;
         this._style  = "plane";
         this._boxes = [];
-        this._font_size = 30;
+        this._fontSize = 30;
 
         for (let i = 0; i < rows + 1; i++){
             this._boxes.push(new MenuBox(x,y + i * height,width,height));
-            this._boxes[i].fontSize = this._font_size;
         }
     }
     rearange(style){
         this._style = style;
 
-        if (style == "plane" ) {        
+        if (style == "plain" ) {        
             for (let i = 1; i < this._boxes.length; i++){
-                this._boxes[i].loadImg("./defaultTextures/menu_mid.png");
+                this._boxes[i].loadImg("../Monkey-Engine/defaultTextures/menu_mid.png");
             }
-            this._boxes[0].loadImg("./defaultTextures/menu_top.png");
-            this._boxes[this._boxes.length - 1].loadImg("./defaultTextures/menu_bot.png");
+            this._boxes[0].loadImg("../Monkey-Engine/defaultTextures/menu_top.png");
+            this._boxes[this._boxes.length - 1].loadImg("../Monkey-Engine/defaultTextures/menu_bot.png");
             this._boxes[this._boxes.length - 1].height = this._height/10;
         }
         else if (style == "socket") {
             for (let i = 0; i < this._boxes.length; i++){
-                this._boxes[i].loadImg("./defaultTextures/menu_top.png");
+                this._boxes[i].loadImg("../Monkey-Engine/defaultTextures/menu_top.png");
             }
-            this._boxes[this._boxes.length - 1].loadImg("./defaultTextures/menu_bot.png");
+            this._boxes[this._boxes.length - 1].loadImg("../Monkey-Engine/defaultTextures/menu_bot.png");
             this._boxes[this._boxes.length - 1].height = this._height/10;       
         }
         else {
@@ -39,20 +38,35 @@ export class Menu {
         }
     }
     render(ctx){
-        const end = new Sprite(this._x,this.y + (this._y * this.rows),this.width,this.height/10,"./defaultTextures/menu_bot.png");
+        const end = new Sprite(this._x,this.y + (this._y * this.rows),this.width,this.height/10,"../Monkey-Engine/defaultTextures/menu_bot.png");
         end.render(ctx);
         for (let i = 0; i < this._boxes.length; i++){
             this._boxes[i].render(ctx);
         }
+    
+    }
+    set fontSize(newFontSize){
+        this._fontSize = newFontSize;
+        for (let i = 0; i < this._boxes.length; i++){
+            this._boxes[i].fontSize = newFontSize
+        }
     }
 }
+/*-----------------------------------------------------------
 const canvas = document.getElementById('herniRozhraní');
 const ctx = canvas.getContext('2d');
 
-const menu = new Menu(500, 300, 360, 80, 3);
-menu.rearange("socket");
-menu._font_size = 30;
-menu._boxes[0].text = "new / delete";
+const menu = new Menu(500, 300, 360, 80, 5);
+menu.rearange("plain");
+menu.fontSize = 40;
+menu._boxes[0].text = "new";
+menu._boxes[0].foldable = true;
+menu._boxes[0].fill = true;
 menu._boxes[1].text = "select";
 menu._boxes[2].text = "info";
+menu._boxes[2].fill = true;
+menu._boxes[3].text = "refactor";
+menu._boxes[3].foldable = true;
+menu._boxes[4].text = "delete";
 menu.render(ctx);
+*/
--- Monkey-Engine/MenuBox.js
+++ Monkey-Engine/MenuBox.js
@@ -2,48 +2,48 @@
 import { Sprite } from "./Sprite.js";
 
 export class MenuBox extends Sprite {
-    constructor(x = 0, y = 0, width, height, texture = "./defaultTextures/infobox.png") {
+    constructor(x = 0, y = 0, width, height, texture = "../Monkey-Engine/defaultTextures/infobox.png") {
         super(x, y, width, height, texture);
-        this._text = "";
-        this._font = "Arial";
-        this._fontSize = 20;
-        this._fontColor = "black";
-        this._textAlign = "left";
-        this._textBaseline = "middle";
-        this._backgroundColor = "magenta";
+        this._text      = "";
+        this._font      = "Arial";
+        this._fontSize  = 30;
+        this._fontColor   = "black";
+        this._textAlign   = "left";//zatim k hovnu
+        this._textBaseline  = "middle"; //zatim k hovnu
+        this._fill = false; 
+        this._foldable = false;
     }
-    render(ctx, Rbox = null) {
-        const AlignWidthRatio = 1/6
-        super.render(ctx, Rbox);
+    render(ctx) {
+        const AlignWidthRatio = 1/9;
+        const BackgroundColor = "pink";
+
+        if (this._fill){
+            ctx.fillStyle = BackgroundColor;
+            ctx.fillRect(this._x,this._y,this._width,this._height);
+        }
+
+        super.render(ctx);
         ctx.font = `${this._fontSize}px ${this._font}`;
         ctx.fillStyle = this._fontColor;
-        ctx.textAlign = this._textAlign;
-        ctx.textBaseline = this._textBaseline;
-        ctx.fillText(this._text, this._x + this._width *  AlignWidthRatio, this._y + this._height / 2);
+        ctx.textAlign = "start";
+        ctx.fillText(this._text, this._x + this._width * AlignWidthRatio, this._y + this._height / 2 + this._fontSize / 2);
+        if (this._foldable){
+            ctx.textAlign = "end"
+            ctx.fillText(">",
+                this._x + this._width - (this._width  *  AlignWidthRatio),
+                this._y + this._height / 2+ this._fontSize / 2
+            )
+            ctx.textAlign = "end"
+
+        }
     }
 
     /*---------------------------Setters-----------------------------*/
-    set text(newText) {
-        this._text = newText;
-    }
-
-    set font(newFont) {
-        this._font = newFont;
-    }
-
-    set fontSize(newFontSize) {
-        this._fontSize = newFontSize;
-    }
-
-    set fontColor(newFontColor) {
-        this._fontColor = newFontColor;
-    }
-
-    set textAlign(newTextAlign) {
-        this._textAlign = newTextAlign;
-    }
-
-    set textBaseline(newTextBaseline) {
-        this._textBaseline = newTextBaseline;
-    }
+    set text(newText) {this._text = newText;}
+    set font(newFont) {this._font = newFont;}
+    set fontSize(newFontSize) {this._fontSize = newFontSize;}
+    set fontColor(newFontColor) {this._fontColor = newFontColor;}
+    set foldable(newFoldable){this._foldable = newFoldable}
+    set fill(newFill){this._fill = newFill;}
+    
 }
