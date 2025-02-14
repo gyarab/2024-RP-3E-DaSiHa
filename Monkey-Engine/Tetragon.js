ef1bf6773cf1fcfd81ccbfadefb552a35991f805
bendl-simon <simon.bendl@student.gyarab.cz>
2025-02-14 12:06:16 +0100

    upgraded rendering, detectors of colision and the example code

--- Monkey-Engine/Tetragon.js
+++ Monkey-Engine/Tetragon.js
@@ -1,22 +1,35 @@
 // Autor: Bendl Šimon
+import {intersectionOfLineSegments} from './LineSection.js';
 export class Tetragon{
+
     constructor(p1, p2, p3, p4, color = 'magenta'){
-        this._points = [p1, p2, p3, p4]
-        this._color  = color
+        this._points = [p1, p2, p3, p4];
+        this._color  = color;
+        this._strokeWidth = 10;
         this._id = Math.floor(Math.random() * 9000) + 1000;
     }
+
+    //! rendering without fill = true will distort 
+    //!   actual size by half of the strokeWidth
     render(ctx, fill) {
         ctx.beginPath();
         ctx.moveTo(this._points[0].x, this._points[0].y);
-        ctx.lineTo(this._points[1].x, this._points[1].y);
-        ctx.lineTo(this._points[2].x, this._points[2].y);
-        ctx.lineTo(this._points[3].x, this._points[3].y);
+        for(let i = 1; i < this._points.length; i++){
+            ctx.lineTo(this._points[i].x, this._points[i].y);
+        }
+        ctx.lineWidth = this._strokeWidth;
         ctx.closePath();
-        //ctx.stroke();
-        //if (fill){
-            ctx.fillStyle = this._color;ctx.fill();
-        //}
+        if(fill){
+            ctx.fillStyle = this._color;
+            ctx.fill();
+        }
+        if(!fill){
+            ctx.strokeStyle = this._color;
+            ctx.lineWidth = this._strokeWidth;
+            ctx.stroke();
+        }
     }
+    ///*Moves the Tetragon to the new position
     moveTo(newX, newY) {
         let v1 = vectorBetween(this._points[0],this._points[1]) 
         let v2 = vectorBetween(this._points[0],this._points[2])
@@ -28,16 +41,46 @@ export class Tetragon{
             {x: newX + v3.x, y: newY + v3.y}
         ]
     }
-    /*----------------------------Setters------------------------- */
+    
+    // * controls if the Tetragon colides with other (Tetragon,...)
+    doesColideWith(other) {
+        //other = Tetragon
+        if (other instanceof Tetragon) {
+            for (let j = 0; j < other._points.length; j++) {
+                for (let i = 0; i < this._points.length; i++) {
+                    let A = this._points[i]; 
+                    let B = this._points[(i + 1) % this._points.length];
+                    let C = other._points[j]; 
+                    let D = other._points[(j + 1) % other._points.length];
+
+                    const intersection = intersectionOfLineSegments(A, B, C, D);
+                    if (colidesAnyPoints(this, other)){return true;}
+                    if (intersection) {
+                        if (intersection === null) {throw new Error("intersectionOfLineSegments() returned null.")}
+                        return true;
+                    }  
+                }
+            }
+        //other != Tetragon
+        }else{
+            throw new Error("Argument is not instance of Tetragon.")
+        }
+        return false;
+    }
+
+    //*------------------Setters--------------------
     set color  (newColor ){ this._color  = newColor }
     set points (newPoints){ this._points = newPoints}
     set id (newId){
-        console.log('id changed from ' + this._id + ' to ' + newId);
+        console.log(
+            '---------------------------------------\n' +
+            'id changed from '+ this._id +' to '+ newId
+        );
         this._id= newId;
-    }  
+    }   
 }
-// TODO: Sakum pikum předělat
-export function colides(tetragon1, tetragon2){
+// * contols if any of the points of the Tetragon intersects the other Tetragon and vice versa
+export function colidesAnyPoints(tetragon1, tetragon2){
     for (let point of tetragon1._points) {
         if (pointInPolygon(point, tetragon2._points)) {return true;}
     }
@@ -46,70 +89,8 @@ export function colides(tetragon1, tetragon2){
     }
     return false;
 }
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! BULLSHIT JE TO VŠECHNO CHYBNÝ A NEFUNGUJE TO
-export function colidesTop(tetragonCollider, tetragon, vicinity){
-    const v = vectorBetween(tetragonCollider._points[0], tetragonCollider._points[1])
-    const pointA = {
-        x: tetragonCollider._points[0].x + (v.x / 2) + vicinity,
-        y: tetragonCollider._points[0].y 
-    }
-    const pointB = {
-        x: tetragonCollider._points[0].x + (v.x / 2) - vicinity,
-        y: tetragonCollider._points[0].y 
-    }
-    if (pointInPolygon(pointA, tetragon._points)){return true;}
-    if (pointInPolygon(pointB, tetragon._points)){return true;}
-    return false;
-}
-export function colidesRight(tetragonCollider, tetragon, vicinity = 0){
-    const v = vectorBetween(tetragonCollider._points[1], tetragonCollider._points[2])
-    const pointA = {
-        x: tetragonCollider._points[1].x ,
-        y: tetragonCollider._points[1].y + (v.y / 2) + vicinity
-    }
-    const pointB = {
-        x: tetragonCollider._points[1].x ,
-        y: tetragonCollider._points[1].y + (v.y / 2) - vicinity
-    }
-    if (pointInPolygon(pointA, tetragon._points)){return true;}
-    if (pointInPolygon(pointB, tetragon._points)){return true;}
-    return false;
-}
-export function colidesBottom(tetragonCollider, tetragon, vicinity = 0){
-    const v = vectorBetween(tetragonCollider._points[2], tetragonCollider._points[3])
-    const pointA = {
-        x: tetragonCollider._points[2].x + (v.x / 2) + vicinity,
-        y: tetragonCollider._points[2].y 
-    }
-    const pointB = {
-        x: tetragonCollider._points[2].x + (v.x / 2) - vicinity,
-        y: tetragonCollider._points[2].y 
-    }
-    if (pointInPolygon(pointA, tetragon._points)){return true;}
-    if (pointInPolygon(pointB, tetragon._points)){return true;}
-    return false;
-}
-export function colidesLeft(tetragonCollider, tetragon, vicinity){
-    const v = vectorBetween(tetragonCollider._points[0],tetragonCollider._points[3])
-    const pointA = {
-        x: tetragonCollider._points[0].x ,
-        y: tetragonCollider._points[0].y + (v.y / 2) + vicinity
-    }
-    const pointB = {
-        x: tetragonCollider._points[0].x ,
-        y: tetragonCollider._points[0].y + (v.y / 2) - vicinity
-    }
-    if (pointInPolygon(pointA, tetragon._points)){return true;}
-    if (pointInPolygon(pointB, tetragon._points)){return true;}
-    return false;
-}
-function vectorBetween(p1 , p2){
-    let vectorX = p2.x - p1.x;
-    let vectorY = p2.y - p1.y;
-    return {x: vectorX, y: vectorY}
-
-}
-function pointInPolygon(point, polygon) {
+// * controls if the point intersects the Polygon
+export function pointInPolygon(point, polygon) {
     let x = point.x, y = point.y;
     let inside = false;
     for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
@@ -120,32 +101,64 @@ function pointInPolygon(point, polygon) {
     }
     return inside;
 }
+function vectorBetween(p1 , p2){
+    let vectorX = p2.x - p1.x;
+    let vectorY = p2.y - p1.y;
+    return {x: vectorX, y: vectorY}
 
-/*-----------------------------Tetragon-----------------------------------
+}
+
+
+/*//*---------------Tetragon-Example-------------------
 const canvas = document.getElementById('herniRozhraní');
 const ctx = canvas.getContext('2d');
-
 const t1 = new Tetragon(
-    {x:  50, y: 200},
-    {x: 100, y: 200},
-    {x: 100, y: 300},
-    {x:  50, y: 300}, 
+    {x:   0, y:   0},
+    {x: 100, y:   0},
+    {x: 400, y: 200},
+    {x:   0, y: 200}, 
     'red'
 );
-
 const t2 = new Tetragon(
-    {x: 250, y: 40},
-    {x: 350, y: 40},
-    {x: 350, y: 240},
-    {x: 250, y: 240},
+    {x:   0, y:   0},
+    {x: 200, y:   0},
+    {x: 200, y: 175},
+    {x:   0, y: 175},
      'blue'
 );    
+const t3 = new Tetragon(
+    {x:   0, y:   0},
+    {x: 100, y:   0},
+    {x: 100, y: 300},
+    {x:   0, y: 300},
+    'green'
+);
+const t4 = new Tetragon(
+    {x:   0, y:  0},
+    {x: 400, y:  0},
+    {x: 300, y: 75},
+    {x:   0, y: 75},
+    'yellow'
+);
 
-t2.moveTo(500,400)
-t1.moveTo(475,599)
+t1.moveTo(100,200)
+t2.moveTo(300,100)
+t3.moveTo(850,500)
+t4.moveTo(700,650)
 
 t2.render(ctx,true);
 t1.render(ctx,true);
-console.log(t1._points[0])
-console.log(colidesTop(t1,t2))
+t3.render(ctx,true);
+t4.render(ctx,true);
+
+console.log(
+    "ColidesAnyPoints pro \n" +
+    "   t1 a t2: " + colidesAnyPoints(t1, t2) + "\n" +
+    "   t3 a t4: " + colidesAnyPoints(t3, t4) + "\n" +
+    "   t1 a t3: " + colidesAnyPoints(t1, t3) + "\n" +
+    "doesColideWith pro \n" +
+    "   t1 a t2: " + t1.doesColideWith(t2) + "\n" +
+    "   t3 a t4: " + t3.doesColideWith(t4) + "\n" +
+    "   t1 a t3: " + t1.doesColideWith(t3)
+);
 /**/
