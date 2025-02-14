90b73617810fd630472dd472c7c9cd163b53e848
bendl-simon <simon.bendl@student.gyarab.cz>
2025-02-14 12:09:18 +0100

    now the rendering of each animation can be slowed

--- Monkey-Engine/CharacterSprite0.js
+++ Monkey-Engine/CharacterSprite0.js
@@ -1,6 +1,6 @@
 import { SpriteDyna } from "./SpriteDyna.js";
 export class CharacterSprite0 extends SpriteDyna{
-    constructor(x, y, width, height, spritePaths = null) {
+    constructor(x, y, width, height, spritePaths) {
         super(x, y, width, height, spritePaths);
 
         this._framesStanding = [];
@@ -9,18 +9,32 @@ export class CharacterSprite0 extends SpriteDyna{
         this._framesRunUp    = [];
         this._framesRunDown  = [];
 
-        const animationSpeed = 1/100;
+        this._animStandingSlow = 10;
+        this._animMovingSlow   = 10;
+
     }    
+    
+    isFor(action){
+        switch(action){
+            case 'GoLeft' :  return (!this._isGoRight  &&  this._isGoLeft);
+            case 'GoRight':  return ( this._isGoRight  && !this._isGoLeft);
+            case 'GoUp'   :  return (!this._isGoDown   &&  this._isGoUp);
+            case 'GoDown' :  return ( this._isGoDown   && !this._isGoUp);
+            case 'Stand'  :  return(
+                (!this._isGoRight && !this._isGoLeft) && 
+                (!this._isGoUp && !this._isGoDown)
+            ); 
+        }
+    }
     render(ctx) {
         let img = null;
-
         if (this.isFor('GoRight')) { 
             img =  this._framesRunRight[
                 (this._currentFrame + this._timeOfAction )% this._framesRunRight.length
             ];
         }  
         if (this.isFor('GoLeft') ) {
-                img = this._framesRunLeft[
+            img = this._framesRunLeft[
                 (this._currentFrame + this._timeOfAction )% this._framesRunLeft.length
             ];
         }
@@ -35,27 +49,34 @@ export class CharacterSprite0 extends SpriteDyna{
             ];
         }
         if (this.isFor('Stand')) {
-                img = this._framesStanding[this._currentFrame];
+            img = this._framesStanding[this._currentFrame];
         }
-        
-
+        console.log(img);
         if (img && img.complete) {
             ctx.drawImage(img, this._x, this._y, this._width, this._height);
-            console.log('drawImage');
         }
     }
-    isFor(action){
-        switch(action){
-            case 'GoLeft' :  return (!this._isGoRight  &&  this._isGoLeft);
-            case 'GoRight':  return ( this._isGoRight  && !this._isGoLeft);
-            case 'GoUp'   :  return (!this._isGoDown   &&  this._isGoUp);
-            case 'GoDown' :  return ( this._isGoDown   && !this._isGoUp);
-            case 'Stand'  :  return (!this._isGoRight  && !this._isGoLeft)||
-                                    ( this._isGoRight  &&  this._isGoLeft);
+    updateImage() {
+        this._animTick += 1;
+        if (this.isFor('Stand')) {
+            if (this._animTick > this._animStandingSlow) {
+                this._currentFrame = (this._currentFrame + 1) % this._framesStanding.length;
+                this._animTick = 0;
+            }
+        } else {
+            if (this._animTick > this._animMovingSlow) {
+                if (this.isFor('GoRight')) {
+                    this._currentFrame = (this._currentFrame + 1) % this._framesRunRight.length;
+                } else if (this.isFor('GoLeft')) {
+                    this._currentFrame = (this._currentFrame + 1) % this._framesRunLeft.length;
+                } else if (this.isFor('GoUp')) {
+                    this._currentFrame = (this._currentFrame + 1) % this._framesRunUp.length;
+                } else if (this.isFor('GoDown')) {
+                    this._currentFrame = (this._currentFrame + 1) % this._framesRunDown.length;
+                }
+                this._animTick = 0;
+            }
         }
-    }
-    updateImage(){
-        this._currentFrame = (this._currentFrame + 1) % this._frames.length;
     }
 }
 /*---------------------------CharacterSprite0-------------------------------
